a843649d606bca9c9825dfec42c9d60ffe14fe7f
CodeGoddess1 <48345161+CodeGoddess1@users.noreply.github.com>
2019-05-14 14:56:03 -0400

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Palindrome.Web
+{
+    public class Palindrome
+    {
+        bool IsPalindrome(string userInput, string userInputReversed)
+        {
+            try
+            {
+                string textInfo = string.Empty;
+                char[] chArray = msg.ToCharArray();
+                char[] PalChar = new char[chArray.Length];
+                PalChar = chArray;
+                textInfo = msg;
+
+                Array.Reverse(PalChar, 0, PalChar.Length);
+                new string(PalChar);
+                if (userInput == userInputReversed)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An Error Occurred");
+                return false;
+
+
+            }
+
+        }
+    }
+}
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Threading.Tasks;
+using Palindrome;
+
+namespace Palindrome.Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string EnterWord = string.Empty;
+            Console.Write("Enter text to see if it’s a palindrome    ");
+            EnterWord = Console.ReadLine();
+
+        }
+
+        protected void UserInPut_TextChanged(object sender, EventArgs e)
+        {
+            UserInPut.Text
+           
+            
+
+        }
+
+        protected void UserOutPut_TextChanged(object sender, EventArgs e)
+        {
+            EnterWordReversed = Paldrm.GetText(EnterWord);
+            string EnterWord = string.Empty;
+            string EnterWordReversed = string.Empty;
+        }
+    }
+}
